89e640c4d7ff486204378ff55742958b51aacfcc
jforjohn <giannisotiro@gmail.com>
2017-07-10 13:00:21 +0200

    CONFIG: add the config file name as a var

--- dbod/config.py
+++ dbod/config.py
@@ -44,32 +44,22 @@ optionalConfig = {
                   }
 
 
-def load( config_file = None ):
+def load(config_file):
     """
     Reads configuration file
     """
     try:
-        parser = argparse.ArgumentParser()
-        parser.add_argument(
-                "-c", "--config", default="/etc/dbod/api.cfg", 
-                help = "specify the location of the config file"
-                )
-        args, unk = parser.parse_known_args()
-
         # Load configuration from file
         config = ConfigParser.ConfigParser()
         config.add_section('tornado')
         config.set('tornado', 'debug', 'false')
-        if config_file:
-            number_read_files = config.read(config_file)
-        else:
-            number_read_files = config.read(args.config)
+        number_read_files = config.read(config_file)
 
         # check the config file exist and can be read
         if len(number_read_files) != 1:
             print "Configuration file '{0}' cannot be read or does not exist. Stopping.".format(args.config)
             sys.exit(1)
-        
+
         # Force load of all required fields to avoid errors in runtime
         for section, options in requiredConfig.items():
             for option in options:
@@ -82,11 +72,20 @@ def load( config_file = None ):
                     print "Option '{0}' not present in section {1} in config file. Stopping.".format(option, section)
                     sys.exit(2)
         return config
-        
+
     except IOError as e:
         traceback.print_exc(file=sys.stdout)
         sys.exit(e.code)
 
 # Loads config
-config = load()
+parser = argparse.ArgumentParser()
+parser.add_argument(
+        "-c", "--config", default="/etc/dbod/api.cfg",
+        help = "specify the location of the config file"
+        )
+args, _ = parser.parse_known_args()
+
+config_file = args.config
+
+config = load(config_file)
 
